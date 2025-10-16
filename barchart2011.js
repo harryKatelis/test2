f87cf5fd608139f41e3d4cfc26ed39b1b56a55ba
harryKatelis <140780709+harryKatelis@users.noreply.github.com>
2025-10-16 23:31:31 +1100

    Add files via upload

---
+++ barchart2011.js
@@ -0,0 +1,5 @@
+var vg_1 = "barchart2011.vg.json";
+vegaEmbed("#barchart2011", vg_1).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
